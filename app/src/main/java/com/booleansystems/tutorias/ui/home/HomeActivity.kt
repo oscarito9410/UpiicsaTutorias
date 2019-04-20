e7790a80d74c4d0b276540a31a4e18b510ff1f83
Oscar Perez MacOs <operez@na-at.com.mx>
2019-04-20 10:58:11 -0500

    -Fixes and search by teachers ans subjects view ready

--- app/src/main/java/com/booleansystems/tutorias/ui/home/HomeActivity.kt
+++ app/src/main/java/com/booleansystems/tutorias/ui/home/HomeActivity.kt
@@ -4,9 +4,12 @@ import android.os.Bundle
 import android.view.View
 import androidx.annotation.IdRes
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import androidx.navigation.Navigation
 import androidx.navigation.fragment.NavHostFragment
 import com.booleansystems.tutorias.R
+import com.booleansystems.tutorias.base.BaseFragment
+import com.booleansystems.tutorias.ui.home.search.SearchFragment
 import kotlinx.android.synthetic.main.activity_home.*
 
 
@@ -15,14 +18,17 @@ class HomeActivity : AppCompatActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(com.booleansystems.tutorias.R.layout.activity_home)
-
         fabSearch.setOnClickListener(this)
-
     }
 
     override fun onClick(v: View?) {
-        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
-        navHostFragment.navController.navigate(R.id.action_newsFragment_to_searchFragment)
+        val ft = supportFragmentManager.findFragmentById(R.id.nav_host_fragment)
+        if (ft !is SearchFragment) {
+            val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+            navHostFragment.navController.navigate(R.id.action_newsFragment_to_searchFragment)
+        }
+
+
     }
 
 
